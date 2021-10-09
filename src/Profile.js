fa59c62df3eabfe0143a6495368887f8702c3bd2
caiovini <caio2791@yahoo.com.br>
2021-10-09 13:21:57 -0300

    Added a few more components

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { ComponentTransition, AnimationTypes } from "react-component-transition";
 import Ship from "./Ship";
 import Stars from "./Stars";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Selfie from "./static/images/selfie.jpeg";
+import Linkedin_selfie from "./static/images/linkedin_selfie.jpeg";
+import Xing_selfie from "./static/images/xing_selfie.jpg";
 import Asteroid from "./Asteroid";
 import LinkedinLogo from "./static/images/linkedin.svg";
 import XingLogo from "./static/images/xing.svg";
 import GithubLogo from "./static/images/github.png";
+
 
 const KEY = {
     LEFT:  37,
@@ -36,7 +45,10 @@ class Profile extends Component{
             },
 
             context: null,
-            destroyAsteroid: false
+            destroyAsteroid: false,
+            showXingPreviewLink: false,
+            showGithutPreviewLink: false,
+            showLinkedinPreviewLink: false
         }
         this.ship = new Ship({
             position: {
@@ -50,19 +62,19 @@ class Profile extends Component{
             new Asteroid({
                 size: 80,
                 position: {
-                x: 100,
+                x: 150,
                 y: this.state.screen.height - 100},
                 id: MEDIA.LINKEDIN}),
             new Asteroid({
                 size: 80,
                 position: {
-                x: this.state.screen.width - 100,
+                x: this.state.screen.width - 150,
                 y: this.state.screen.height - 100},
                 id: MEDIA.GITHUB}),
             new Asteroid({
                 size: 80,
                 position: {
-                x: this.state.screen.width - 100,
+                x: this.state.screen.width - 150,
                 y: 100},
                 id: MEDIA.XING})
         ];
@@ -104,17 +116,17 @@ class Profile extends Component{
 
                 case MEDIA.LINKEDIN:
                     asteroid.setPosition({
-                        x: 100,
+                        x: 150,
                         y: this.state.screen.height - 100})
                     break;
                 case MEDIA.GITHUB:
                     asteroid.setPosition({
-                        x: this.state.screen.width - 100,
+                        x: this.state.screen.width - 150,
                         y: this.state.screen.height - 100})
                     break;
                 case MEDIA.XING:
                     asteroid.setPosition({
-                        x: this.state.screen.width - 100,
+                        x: this.state.screen.width - 150,
                         y: 100})
                     break;
                 default:
@@ -227,6 +239,48 @@ class Profile extends Component{
         return false;
     }
 
+    toggleHoverMouseEnter(e, media){
+
+        e.preventDefault();
+        switch (media){
+
+            case MEDIA.LINKEDIN:
+                this.setState({showLinkedinPreviewLink:true});
+                break;
+            case MEDIA.GITHUB:
+                this.setState({showGithutPreviewLink:true});
+                break;
+            case MEDIA.XING:
+                this.setState({showXingPreviewLink:true});
+                break;
+            default:
+                break;
+
+        }
+        
+    }
+
+    toggleHoverMouseLeave(e, media) {
+      
+        e.preventDefault();
+        switch (media){
+
+            case MEDIA.LINKEDIN:
+                this.setState({showLinkedinPreviewLink:false});
+                break;
+            case MEDIA.GITHUB:
+                this.setState({showGithutPreviewLink:false});
+                break;
+            case MEDIA.XING:
+                this.setState({showXingPreviewLink:false});
+                break;
+            default:
+                break;
+
+        }
+  
+      }
+
     render(){
         
 
@@ -241,43 +295,150 @@ class Profile extends Component{
                     case MEDIA.LINKEDIN:
                         badges.push(
 
-                            <a href="https://www.linkedin.com/in/caiovinireis/" 
-                               key={MEDIA.LINKEDIN}
-                               target="_blank" 
-                               rel="noreferrer">
-                                <img className="badges linkedin"
-                                    src={LinkedinLogo}
-                                    alt="Linkedin"
-                                />
-                            </a>
+                            <div className="badges linkedin" 
+                                onMouseEnter={ (e) => this.toggleHoverMouseEnter(e, MEDIA.LINKEDIN)}
+                                onMouseLeave={ (e) => this.toggleHoverMouseLeave(e, MEDIA.LINKEDIN)}
+                                key={MEDIA.LINKEDIN} >
+                                <a href="https://www.linkedin.com/in/caiovinireis/?locale=en_US" 
+                                target="_blank" 
+                                rel="noreferrer">
+                                    <img className="badge-effect"
+                                        src={LinkedinLogo}
+                                        alt="Linkedin"
+                                    />
+                                </a>
+                                <ComponentTransition
+                                    exitAnimation={AnimationTypes.fade.exit}
+                                >
+                                { this.state.showLinkedinPreviewLink ?  
+                                    <Card style={{backgroundColor: "rgba(255, 255, 255, 0.9)" ,
+                                          opacity: 0.9, bottom: 160, left: 170, position: "fixed"}} 
+                                        sx={{ maxWidth: 300, 
+                                        color: 'background.paper',
+                                        boxShadow: 1,
+                                        borderRadius: 1,
+                                        p: 2,
+                                        minWidth: 300,}}> 
+                                        <CardMedia
+                                            component="img"
+                                            height="194"
+                                            image={Linkedin_selfie}
+                                            alt="Paella dish"
+                                        />
+                                        <CardContent >
+                                            <Typography variant="body2" color="text.primary">
+                                                View my profile on LinkedIn, the world’s largest professional community.
+                                                I have listed my experiences as well as some articles I wrote related to blockchain
+                                                which is a subject I am deeply interested.<br/><br/>
+                                                <a href="https://www.linkedin.com/in/caiovinireis/?locale=en_US" 
+                                                   target="_blank"
+                                                   rel="noreferrer">
+                                                Check it out here </a>
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>      
+                                : "" }
+                                </ComponentTransition>
+                            </div>
                         );
                         break;
                     case MEDIA.GITHUB:
                         badges.push(
 
-                            <a href="https://github.com/caiovini" 
-                               key={MEDIA.GITHUB}
-                               target="_blank"
-                               rel="noreferrer">
-                                <img className="badges github"
-                                    src={GithubLogo}
-                                    alt="Github"
-                                />
-                            </a>
+                            <div className="badges github" 
+                                onMouseEnter={ (e) => this.toggleHoverMouseEnter(e, MEDIA.GITHUB)}
+                                onMouseLeave={ (e) => this.toggleHoverMouseLeave(e, MEDIA.GITHUB)}
+                                key={MEDIA.GITHUB}>
+                                <a href="https://github.com/caiovini" 
+                                target="_blank"
+                                rel="noreferrer">
+                                    <img className="badge-effect"
+                                        src={GithubLogo}
+                                        alt="Github"
+                                    />
+                                </a>
+                                <ComponentTransition
+                                    exitAnimation={AnimationTypes.fade.exit}
+                                >
+                                { this.state.showGithutPreviewLink ?  
+                                    <Card style={{backgroundColor: "rgba(255, 255, 255, 0.9)", 
+                                            opacity: 0.9, bottom: 150, right: 175, position: "fixed"}} 
+                                        sx={{ maxWidth: 300,
+                                        color: 'background.paper',
+                                        boxShadow: 1,
+                                        borderRadius: 1,
+                                        p: 2,
+                                        minWidth: 300,}}> 
+                                        <CardMedia
+                                            component="img"
+                                            height="194"
+                                            image={Selfie}
+                                            alt="Paella dish"
+                                        />
+                                        <CardContent >
+                                            <Typography variant="body2" color="text.primary">
+                                                Please view my profile on github, this is where I put all
+                                                my personal applications, including a few games which are one of my passions.<br/><br/>
+                                                <a href="https://github.com/caiovini" 
+                                                   target="_blank"
+                                                   rel="noreferrer">
+                                                Check it out here </a>
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                : "" }
+                                </ComponentTransition>
+                            </div>
                         );
                         break;
                     case MEDIA.XING:
                         badges.push(
-
-                            <a href="https://www.xing.com/profile/CaioVinicius_NascimentoReis" 
-                               key={MEDIA.XING} 
-                               target="_blank"
-                               rel="noreferrer">
-                                <img className="badges xing"
-                                    src={XingLogo}
-                                    alt="Xing"
-                                />
-                            </a>
+                            
+                            <div className="badges xing" 
+                                onMouseEnter={ (e) => this.toggleHoverMouseEnter(e, MEDIA.XING)}
+                                onMouseLeave={ (e) => this.toggleHoverMouseLeave(e, MEDIA.XING)}
+                                key={MEDIA.XING} >
+                                <a href="https://www.xing.com/profile/CaioVinicius_NascimentoReis" 
+                                target="_blank"
+                                rel="noreferrer">
+                                    <img className="badge-effect"
+                                        src={XingLogo}
+                                        alt="Xing"
+                                    />
+                                    
+                                </a>
+                                <ComponentTransition
+                                    exitAnimation={AnimationTypes.fade.exit}
+                                >
+                                { this.state.showXingPreviewLink ?  
+                                    <Card style={{backgroundColor: "rgba(255, 255, 255, 0.9)",
+                                            opacity: 0.9, right: 150, position: "fixed" }} 
+                                        sx={{ maxWidth: 300,
+                                        color: 'background.paper',
+                                        boxShadow: 1,
+                                        borderRadius: 1,
+                                        p: 2,
+                                        minWidth: 300,}} > 
+                                        <CardMedia
+                                            component="img"
+                                            height="194"
+                                            image={Xing_selfie}
+                                            alt="Paella dish"
+                                        />
+                                        <CardContent >
+                                            <Typography variant="body2" color="text.primary">
+                                                This is my xing, profile, feel free to have a look. I have all my experience listed here,
+                                                xing is the largest online business network in Germany, Austria, and Switzerland.<br/><br/>
+                                                <a href="https://www.xing.com/profile/CaioVinicius_NascimentoReis" 
+                                                   target="_blank"
+                                                   rel="noreferrer">
+                                                Check it out here </a>
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                : "" }
+                                </ComponentTransition>
+                            </div>
                         );
                         break;
                     default:
@@ -292,15 +453,33 @@ class Profile extends Component{
             <div>
                 { badges }
                 <span className="avatar" >
-                    <Avatar
-                        alt="Caio Vinicius"
-                        src={Selfie}
-                        sx={{ width: 180, height: 180 }}
-                    />   
-                    <br/>        
-                    Salute,<br/> My name is Caio, I am<br/> a software engineer from Brazil.<br/>
-                    Please, hit the asteroids to reveal<br/> my social media links and feel free<br/>
-                    to have a look at my personal projects.
+                    <Card style={{backgroundColor: "rgba(0, 0, 0, 0)" , opacity: 0.9}} 
+                          sx={{ maxWidth: 450,
+                          color: 'background.paper',
+                          boxShadow: 1,
+                          borderRadius: 1,
+                          p: 2,
+                          minWidth: 300,}}>
+                        <CardHeader
+                        avatar={
+                            <Avatar
+                                alt="Caio Vinicius"
+                                src={Selfie}
+                                sx={{ width: 120, height: 120 }}
+                            />   
+                        }
+                        
+                          title="Caio Vinicius Nascimento Reis"
+                        />
+                        <CardContent >
+                            <Typography>
+                                    Salute,<br/>
+                                    My name is Caio, I am a software engineer from Brazil.
+                                    Please, hit the asteroids to reveal my social media links and feel free
+                                    to have a look at my personal projects.
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </span>   
                 <span className="controls" >
                     Use [←][↑][→] to MOVE<br/>
