98c867d14a9f3de0d021e46a9208dd40f2781cc5
caiovini <caio2791@yahoo.com.br>
2021-10-14 21:55:24 -0300

    Added effects to stars

--- src/Stars.js
+++ src/Stars.js
@@ -21,6 +21,7 @@ export default class Stars{
 
         this.speed = 2;
         this.boost = false;
+        this.starMomentum = 0;
     }
 
     getRandomNumber(min, max){
@@ -35,9 +36,22 @@ export default class Stars{
 
     render(context, angle, screen){
 
-        this.speed = this.boost ? 4 : 2;
+        this.speed = this.boost ? 6 : 2;
+        this.starMomentum = this.boost ? this.starMomentum === 25 ? this.starMomentum 
+                : this.starMomentum + 0.5 : this.speed+1;
 
         this.parms.forEach(star =>{
+
+            if (star.position.x > screen.width) { star.position.x = 0; }
+            if (star.position.x < 0) { star.position.x = screen.width; }
+            if (star.position.y > screen.height){ star.position.y = 0; }
+            if (star.position.y < 0){ star.position.y = screen.height; }
+
+            context.beginPath();
+            context.moveTo(star.position.x - 
+                                Math.sin(angle * Math.PI / 180) * this.starMomentum, 
+                           star.position.y + 
+                                Math.cos(angle * Math.PI / 180) * this.starMomentum);
 
             star.position.x = star.position.x - 
                 Math.sin(angle * Math.PI / 180) * this.speed;
@@ -45,15 +59,10 @@ export default class Stars{
             star.position.y = star.position.y + 
                 Math.cos(angle * Math.PI / 180) * this.speed;
 
-            if (star.position.x > screen.width) { star.position.x = 0; }
-            if (star.position.x < 0) { star.position.x = screen.width; }
-            if (star.position.y > screen.height){ star.position.y = 0; }
-            if (star.position.y < 0){ star.position.y = screen.height; }
+            
 
-            
-            
-            context.beginPath();
-            context.arc(star.position.x, star.position.y, 1, 0, 2 * Math.PI);
+            context.lineTo(star.position.x, star.position.y);
+            //context.arc(star.position.x, star.position.y, 0.5, 0, 2 * Math.PI);
             context.strokeStyle = star.strokeStyle;
             context.fillStyle = star.fillStyle;
             context.lineWidth = star.lineWidth;
